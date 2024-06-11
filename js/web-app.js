f31c85d1e8995a1ea75c14fa6805f010188ddb80
Emmett922 <emmett.peters922@gmail.com>
2024-06-11 13:46:12 -0400

    Saving progress

--- js/web-app.js
+++ js/web-app.js
@@ -13,4 +13,16 @@ $(document).ready(function() {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    // -- Toggle Services Description pop-up -- //
+    // Opening pop-up
+    $('.services .services-content .card').click(function($e) {
+        $('.services .services-content .card .serv-descr').addClass("active");
+        $('.services .services-content .card').addClass("active");
+
+        $('.services .services-content .card .serv-descr .close').click(function() {
+            $('.services .services-content .card .serv-descr').removeClass("active");
+            $('.services .services-content .card').removeClass("active");
+        });
+    });
 });
